477c4594b38fd8c1ac6bcb3dc467d99d511f5b3f
Phan Công Thức <pct@netcompany.com>
2022-12-10 23:43:22 +0700

    Initial commit

---
+++ src/main/java/pct/skedulo/App.java
@@ -0,0 +1,13 @@
+package pct.skedulo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
