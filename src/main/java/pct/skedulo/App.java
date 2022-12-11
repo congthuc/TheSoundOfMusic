5fe4d85fc5fd83f05a7361a2e6fd76427ae902ee
Thuc Phan <congthuc.uit@gmail.com>
2022-12-11 16:17:35 +0700

    Add file not found check

--- src/main/java/pct/skedulo/App.java
+++ src/main/java/pct/skedulo/App.java
@@ -3,6 +3,8 @@ package pct.skedulo;
 import pct.skedulo.model.Show;
 import pct.skedulo.process.DataProcessor;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
 
@@ -11,6 +13,10 @@ public class App
     public static void main( String[] args ) throws IOException {
         String filePath = args[0];
         System.out.println( "File Path: " + filePath);
+        if (! new File(filePath).exists()) {
+            System.out.println(filePath + " is not exist!");
+            throw new FileNotFoundException(filePath + " is not exist!");
+        }
 
         DataProcessor dataProcessor = new DataProcessor();
         List<Show> shows = dataProcessor.loadShowsFromFile(filePath, "application/json");
